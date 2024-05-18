e7bb3c58be22b63670a180c77866f737815aacd1
Gusales <dev.gussales@gmail.com>
2024-05-18 13:22:04 -0300

    feat(server): configure environment variables

--- server/db.py
+++ server/db.py
@@ -1,9 +1,14 @@
 import mysql.connector
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
 
 def connectDatabase():
   return mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='',
-    database='painel_database'
+    host=os.getenv('DATABASE_HOST'),
+    user=os.getenv('DATABASE_USER'),
+    password=os.getenv('DATABASE_PASSWORD'),
+    database=os.getenv('DATABASE_D')
   )
