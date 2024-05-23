4c88e77983edcebf72375c0c65dd9a94240f69ac
Gusales <dev.gussales@gmail.com>
2024-05-23 00:38:21 -0300

    Criação do algorítmo para reordenação de fila a partir de sua preferencial

---
+++ server/utils/reorder_queue_by_priority.py
@@ -0,0 +1,45 @@
+def organizeQueue(passwords):
+  def splitPassword(password):
+    priorityLevel = password.split(".")[0]
+    urgencyLevel = password.split(".")[1]
+    callOrder = int(password.split(".")[2])
+
+    return priorityLevel, urgencyLevel, callOrder
+  
+  urgency_order = {
+    'E': 0,
+    'U': 1,
+    'P': 2,
+    'N': 3 
+  }
+
+  def password_sort_key(password):
+    priority, urgency, number = splitPassword(password)
+    return (priority != 'P', urgency_order[urgency], number)
+  
+  reorderQueue = sorted(passwords, key=password_sort_key)
+
+  priorityPasswords = []
+  normalPasswords = []
+  reordered = []
+
+  for senha in reorderQueue:
+    priorityPasswords.append(senha) if senha.split(".")[0] == "P" else normalPasswords.append(senha)
+
+  lengthPreferential = len(priorityPasswords)
+  lengthNormal = len(normalPasswords)
+
+  indexP = 0 
+  indexN = 0
+  while indexP < lengthPreferential or indexN < lengthNormal:
+    if indexP < lengthPreferential:
+      reordered.append(priorityPasswords[indexP])
+      indexP += 1
+    if indexP < lengthPreferential:
+      reordered.append(priorityPasswords[indexP])
+      indexP += 1
+    if indexN < lengthNormal:
+      reordered.append(normalPasswords[indexN])
+      indexN += 1
+
+  return reordered
